2edb5bfefa68d3f425b09fb345f3e719ba099e9f
Hanna <hanna@prevetfamily.com>
2025-11-02 18:52:47 +0100

    Add basic sections in home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
 export default function Home() {
   return (
     <div
-      className="flex min-h-screen items-center justify-center bg-zinc-50 
-    font-sans dark:bg-black"
+      className="flex min-h-screen items-center justify-center 
+    "
     >
       <main
         className="flex min-h-screen w-full max-w-6xl flex-col items-center 
-      justify-between py-32 px-16 bg-white dark:bg-black "
+      justify-between py-32 px-16  "
       >
-        <h1 className="text-center text-5xl">Hannas portfolio</h1>
+        <section>
+          <h1
+            className="text-center text-5xl font-black 
+          text-(--color-text-secondary)"
+          >
+            Nice to meet you! I&apos;m Hannna Klang Eriksson.
+          </h1>
+        </section>
+
+        <section id="projects" className="py-10">
+          Projects
+        </section>
+        <section id="about" className="py-10">
+          About
+        </section>
+        <section id="contact" className="py-10">
+          Contact
+        </section>
       </main>
     </div>
   );
