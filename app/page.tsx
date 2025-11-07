cdba9737c96f14349e04ec514827388ee707a515
Hanna <hanna@prevetfamily.com>
2025-11-07 08:46:47 +0100

    Use contact section in page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
+import ContactSection from "./components/ContactSection";
 import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen w-full flex-col items-center overflow-x-hidden
-        "
+      className="flex min-h-screen w-full flex-col items-center 
+      overflow-x-hidden"
     >
       <HeroSection />
       <ProjectsSection />
-
+      <ContactSection />
       {/*  <section id="about" className="py-10">
         <h2 className="font-serif-custom text-5xl">About</h2>
       </section>
-      <section id="contact" className="py-10">
-        <h2 className="font-serif-custom text-5xl">Contact me</h2>
-      </section> */}
+      */}
     </main>
   );
 }
