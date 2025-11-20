e824ea881ad3b6455bf68135cbb126deeccc056d
Hanna <hanna@prevetfamily.com>
2025-11-20 16:30:50 +0100

    Refractor contact section, change text styles.

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -1,31 +1,27 @@
 "use client";
 
+import PrimaryButtonLink from "./common/PrimaryButtonLink";
+
 const email = "hanna.klang.eriksson@gmail.com";
 
 export default function ContactSection() {
   return (
-    <section id="contact" className="py-12 max-w-6xl mx-auto px-5 ">
+    <section id="contact" className="py-10 max-w-6xl mx-auto px-5 ">
       <div className="flex flex-col items-center">
-        <h2 className="font-serif-custom text-4xl lg:text-5xl tracking-tighter">
+        <h2
+          className="font-serif-custom text-4xl lg:text-5xl tracking-tight 
+        font-semibold text-(--color-text-secondary) mb-10"
+        >
           Get in touch
         </h2>
-        <p className="mt-8 sm:w-3/4 lg:w-5/9 text-center leading-relaxed">
+        <p className="mb-10 sm:w-3/4 lg:w-5/9 text-center leading-relaxed">
           I’m currently open to new opportunities. Feel free to reach out with
           projects, feedback, or any questions. I’d love to hear from you!
         </p>
-        <div className="mt-8 mb-8">
-          <a
-            href={`mailto:${email}`}
-            className="bg-(--color-text-secondary) border 
-            border-(--color-text-secondary)
-            px-6 py-3 rounded-full text-(--color-text-light)
-            transition-all duration-300 ease-in-out 
-            hover:cursor-pointer hover:bg-transparent
-            hover:text-black hover:border-(--color-text-secondary)
-            font-semibold tracking-wide inline-block text-center"
-          >
+        <div className="mb-10">
+          <PrimaryButtonLink href={`mailto:${email}`}>
             Email me
-          </a>
+          </PrimaryButtonLink>
         </div>
       </div>
     </section>
