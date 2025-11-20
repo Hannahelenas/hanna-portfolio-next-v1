dfc37c3fe7d7afacb21ca1082079a25a90cc3213
Hanna <hanna@prevetfamily.com>
2025-11-20 16:26:49 +0100

    Refractor styling in projects section.

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -4,15 +4,16 @@ import { projects } from "@/data/projects";
 
 export default function ProjectsSection() {
   return (
-    <section id="projects" className="py-10 max-w-6xl mx-auto">
+    <section id="projects" className="py-10 max-w-5xl mx-auto">
       {" "}
       <h2
-        className=" text-4xl lg:text-5xl text-center mb-4 
-      font-serif-custom tracking-tighter"
+        className="text-4xl lg:text-5xl text-center mb-12
+      font-serif-custom leading-tight tracking-tight font-semibold 
+      text-(--color-text-secondary)"
       >
         Projects
       </h2>
-      <ul className="grid grid-cols-1 lg:grid-cols-2 gap-1 lg:gap-6">
+      <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2 sm:gap-4 lg:gap-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
