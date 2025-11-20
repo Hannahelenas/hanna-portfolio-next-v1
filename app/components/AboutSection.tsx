9b7bc95cbaeae7cbf49ac804c7b189f980e2b2ae
Hanna <hanna@prevetfamily.com>
2025-11-20 16:29:48 +0100

    Refractor text styles and margins in about and skills section.

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 export default function AboutSection() {
   return (
     <div className="bg-(--color-bg-secondary) w-full">
-      <section id="about" className="py-12 max-w-4xl mx-auto px-5 sm:px-10">
+      <section id="about" className="py-10 max-w-4xl mx-auto px-5 sm:px-10">
         <h2
-          className="font-serif-custom text-4xl lg:text-5xl tracking-tighter 
-        mb-15  text-center"
+          className="text-4xl lg:text-5xl text-center mb-10 font-serif-custom 
+          tracking-tight font-semibold text-(--color-text-secondary)"
         >
           About me
         </h2>
         <h3
-          className="font-serif-custom text-4xl lg:text-4xl tracking-tighter 
-        mb-4"
+          className="font-serif-custom text-2xl md:text-3xl font-semibold 
+        tracking-tight"
         >
           Hanna Klang Eriksson
         </h3>
@@ -31,31 +31,30 @@ export default function AboutSection() {
           </div>
 
           {/* Text floating around image */}
-          <p className="mb-4">
+          <p className="mb-4 mt-4">
             Nice to meet you, I´m Hanna! I am a Frontend developer based in
             Uppsala, Sweden. My interest for programming started when I began to
             do small projects in my sparetime while working as a nurse and soon
             it grew from a hobby to a desired career.
           </p>
-          <p>
+          <p className="mb-4">
             {" "}
             During my studies in Front-end development I discovered my passion
             for creating inclusive and accessible user experiences, which is
             something I am genuinely dedicated to and thriving to learn more
             about.
           </p>
-          <p className="mt-2">
+          <p className="mb-4">
             {" "}
             What I love about coding is that there are so many ways to be
             creative, solve problems and of course that you can always learn
             something new!{" "}
           </p>
 
-          <p className="text-lg font-serif-custom font-semibold mt-4">
-            &quot;All development starts with communication. I bring a genuine
-            warmth to every project, believing in teamwork. My passion lies in
-            crafting impactful solutions, with empathy and a dedicated focus on
-            the people who will use them.&quot;
+          <p className="mt-4">
+            I bring genuine warmth to my projects and believe in the power of
+            teamwork. My passion lies in crafting impactful solutions with
+            empathy and focus on the people who will use them
           </p>
         </div>
       </section>
