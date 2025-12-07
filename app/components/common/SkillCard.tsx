e5c7b9f960303d400277eaa680f5bb0f7469e8b4
Hanna <hanna@prevetfamily.com>
2025-12-07 18:05:39 +0100

    Change heading order in skill card component.

--- app/components/common/SkillCard.tsx
+++ app/components/common/SkillCard.tsx
@@ -10,7 +10,7 @@ export default function SkillCard({ title, skills }: SkillCardProps) {
       className="p-6 border rounded-lg border-(--color-accent-primary) 
     bg-(--color-card-bg)"
     >
-      <h4 className="text-xl font-serif-custom mb-4 font-semibold">{title}</h4>
+      <h3 className="text-xl font-serif-custom mb-4 font-semibold">{title}</h3>
       <ul className="flex flex-wrap gap-x-2 gap-y-5 mt-5 mb-2">
         {skills.map((skill) => (
           <li className="list-none text-sm " key={skill}>
